421abaf5216e37867c85b4450d7bbdb46aefa789
viktoria <kingsviktory@gmail.com>
2025-05-19 17:33:11 +0300

    feat: profile card component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProfileCardComponent } from './common-ui/profile-card/profile-card.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProfileCardComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'tick-talk';
+  title = 'World';
+  green = 'green';
+  items = [
+    { title: 'Explore the Docs', link: 'https://angular.dev' },
+    { title: 'Learn with Tutorials', link: 'https://angular.dev/tutorials' },
+    { title: 'CLI Docs', link: 'https://angular.dev/tools/cli' },
+    {
+      title: 'Angular Language Service',
+      link: 'https://angular.dev/tools/language-service',
+    },
+    { title: 'Angular DevTools', link: 'https://angular.dev/tools/devtools' },
+  ];
 }
