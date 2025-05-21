9a290b8decff2ec68ea7206d3ac47f39af3d037b
viktoria <kingsviktory@gmail.com>
2025-05-21 18:48:07 +0300

    feat: login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,17 @@
 import { Routes } from '@angular/router';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
+import { LayoutComponent } from './common-ui/layout/layout.component';
 
 export const routes: Routes = [
-  { path: '', component: SearchPageComponent },
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: '', component: SearchPageComponent },
+      { path: 'profile', component: ProfilePageComponent },
+    ],
+  },
   { path: 'login', component: LoginPageComponent },
 ];
