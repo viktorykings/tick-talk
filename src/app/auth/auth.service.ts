5687fa04c16b03a1e403a018f600d1455cc11730
viktoria <kingsviktory@gmail.com>
2025-05-22 23:00:43 +0300

    feat: store token in cookies

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { tap } from 'rxjs';
 import { ITokenResponse } from './auth.interface';
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
   providedIn: 'root',
@@ -9,10 +10,15 @@ import { ITokenResponse } from './auth.interface';
 export class AuthService {
   http = inject(HttpClient);
   baseURL = 'https://icherniakov.ru/yt-course/auth/';
+  cookieService = inject(CookieService);
+
   token: string | null = null;
   refreshToken: string | null = null;
 
   get isAuth() {
+    if (!this.token) {
+      this.token = this.cookieService.get('token');
+    }
     return !!this.token;
   }
 
@@ -24,6 +30,8 @@ export class AuthService {
       tap((val) => {
         this.token = val.access_token;
         this.refreshToken = val.refresh_token;
+        this.cookieService.set('token', this.token);
+        this.cookieService.set('refreshToken', this.refreshToken);
       })
     );
   }
